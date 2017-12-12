13dd2113259cad3f0f994d26597da5f416dac0c8
Magnus Strömbrink <magnus.stroembrink@vattenfall.com>
2017-12-12 17:11:07 +0100

    Splitted Village into Villa + Program

--- day12/Village.go
+++ day12/Village.go
@@ -8,32 +8,6 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-type Program struct {
-	ID    int
-	Pipes []*Program
-}
-
-func (p *Program) AddPipe(child *Program) {
-	p.Pipes = append(p.Pipes, child)
-}
-
-func (p *Program) Group() map[int]*Program {
-	group := make(map[int]*Program, 0)
-	p._group(group)
-	//
-	//
-	return group
-}
-
-func (p *Program) _group(g map[int]*Program) {
-	if _, ok := g[p.ID]; !ok {
-		g[p.ID] = p
-		for _, c := range p.Pipes {
-			c._group(g)
-		}
-	}
-}
-
 type Village struct {
 	Programs []*Program
 }
